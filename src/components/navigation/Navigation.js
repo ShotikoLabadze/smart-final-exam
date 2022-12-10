2c2b6f2ebda5d1b38a5aa485402b6fe529bde525
ShotaL <52139226+ShotikoLabadze@users.noreply.github.com>
2022-12-10 04:06:49 +0400

    changed routing

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,41 +1,28 @@
 import React from "react";
 import { useState } from "react";
-import { Link, useResolvedPath, useMatch } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Navigation.css";
 
 function Navigation() {
   const [active, setActive] = useState("");
   return (
     <nav>
-      <CustomLink
+      <Link
         to="/about"
         onClick={() => setActive("#about")}
         className={active === "#about" ? "active" : ""}
       >
         about
-      </CustomLink>
-      <CustomLink
+      </Link>
+      <Link
         to="/homepage"
         onClick={() => setActive("#homepage")}
         className={active === "#homepage" ? "active" : ""}
       >
         homepage
-      </CustomLink>
+      </Link>
     </nav>
   );
 }
 
 export default Navigation;
-
-function CustomLink({ to, children, ...props }) {
-  const resolvedPath = useResolvedPath(to);
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
-
-  return (
-    <li className={isActive ? "active" : ""}>
-      <Link to={to} {...props}>
-        {children}
-      </Link>
-    </li>
-  );
-}
